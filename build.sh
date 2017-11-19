7134edbefe8af3aea81664f8342de3ed5cd800c8
khs1994 <khs1994@khs1994.com>
2017-11-19 14:17:07 +0800

    Update Hexo 3.4.1

--- build.sh
+++ build.sh
@@ -2,20 +2,32 @@
 
 DOCS_REPO=https://github.com/khs1994-website/docs.git
 PHP_DOCS_REPO=https://github.com/khs1994-website/php-docs.git
+GO_DOCS_REPO=https://github.com/khs1994-website/go-docs.git
 REPO=https://github.com/khs1994/khs1994.github.io.git
 
-if [ "$1" = "git" ];then
-  git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
-  git clone -b master "$REPO" .deploy_git
-  if [ ! $? = 0 ];then echo -e "\033[32mINFO\033[0m  BRANCH NOT exist"; else echo -e "\033[32mINFO\033[0m  BRANCH exist"; fi
+if [ "$1" = git ];then
+  git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file" > /dev/null 2>&1; done
+  # git clone -b master "$REPO" .deploy_git
+  # if [ ! $? = 0 ];then echo -e "\033[32mINFO\033[0m  BRANCH NOT exist"; else echo -e "\033[32mINFO\033[0m  BRANCH exist"; fi
+  rm -rf source/_posts
   git clone -b gitbook --depth=1 "$DOCS_REPO" source/_posts
   cd source/_posts
   mv img ../
-  git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
+  git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file" > /dev/null 2>&1; done
+
   rm -rf php
   git clone -b gitbook --depth=1 "$PHP_DOCS_REPO" php
   cd php
-  git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
+  git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file" > /dev/null 2>&1; done
+  rm -rf _book node_modules .DS_Store *.json *.md .deploy_git
+  cd ..
+
+  rm -rf golang
+  git clone -b gitbook --depth=1 "$GO_DOCS_REPO" golang
+  cd golang
+  git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file" > /dev/null 2>&1; done
+  rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt
+  cd ..
   exit 0
 fi
 
@@ -28,15 +40,22 @@ cp -a source/_posts/img source/img/
 cd source/_posts
 rm -rf _book node_modules .DS_Store *.json *.md .deploy_git
 
-echo -e "\033[32mINFO\033[0m  Remove php folder[$PWD/php]"
-
+echo -e "\033[32mINFO\033[0m  Remove PHP folder[$PWD/php]"
 rm -rf php/*
-cp -a -p ../../../php-docs/* php/
-
+cp -a ../../../php-docs/* php/
 echo -e "\033[32mINFO\033[0m  Success move php-source TO [$PWD/php]"
-
 cd php
+rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt
+cd ..
+
+echo -e "\033[32mINFO\033[0m  Remove Golang folder[$PWD/golang]"
+rm -rf golang/*
+cp -a ../../../go-docs/* golang/
+echo -e "\033[32mINFO\033[0m  Success move go-source TO [$PWD/golang]"
+cd golang
 rm -rf _book node_modules .DS_Store *.json *.md .deploy_git
-cd ../../
+cd ..
+
+cd ..
 echo
 hexo clean
