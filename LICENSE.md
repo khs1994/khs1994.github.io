1991d20e6a86c543a58a068c74eee62219b9b14c
khs1994 <khs1994@khs1994.com>
2017-12-06 22:59:59 +0800

    Update 3.4.3 [skip ci]

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+ 所有文章除特别声明外，均采用 [`CC BY-NC-SA 3.0`](https://creativecommons.org/licenses/by-nc-sa/3.0/deed.zh) 许可协议。
--- build.sh
+++ build.sh
@@ -14,19 +14,20 @@ if [ "$1" = git ];then
   cd source/_posts
   mv img ../
   git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file" > /dev/null 2>&1; done
+  rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt .github
 
   rm -rf php
   git clone -b gitbook --depth=1 "$PHP_DOCS_REPO" php
   cd php
   git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file" > /dev/null 2>&1; done
-  rm -rf _book node_modules .DS_Store *.json *.md .deploy_git
+  rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt .github
   cd ..
 
   rm -rf golang
   git clone -b gitbook --depth=1 "$GO_DOCS_REPO" golang
   cd golang
   git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file" > /dev/null 2>&1; done
-  rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt
+  rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt .github
   cd ..
   exit 0
 fi
@@ -38,14 +39,14 @@ echo -e "\033[32mINFO\033[0m  Copy post source..."
 cp -a  ../docs/* source/_posts/
 cp -a source/_posts/img source/img/
 cd source/_posts
-rm -rf _book node_modules .DS_Store *.json *.md .deploy_git
+rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt .github
 
 echo -e "\033[32mINFO\033[0m  Remove PHP folder[$PWD/php]"
 rm -rf php/*
 cp -a ../../../php-docs/* php/
 echo -e "\033[32mINFO\033[0m  Success move php-source TO [$PWD/php]"
 cd php
-rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt
+rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt .github
 cd ..
 
 echo -e "\033[32mINFO\033[0m  Remove Golang folder[$PWD/golang]"
@@ -53,7 +54,7 @@ rm -rf golang/*
 cp -a ../../../go-docs/* golang/
 echo -e "\033[32mINFO\033[0m  Success move go-source TO [$PWD/golang]"
 cd golang
-rm -rf _book node_modules .DS_Store *.json *.md .deploy_git
+rm -rf _book node_modules .DS_Store *.json *.md .deploy_git *.txt .github
 cd ..
 
 cd ..
